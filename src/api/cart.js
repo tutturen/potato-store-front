e2e30870127fe51c539551298d5541faf9afed3c
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-03-02 15:26:16 +0100

    First attempt at cart API

---
+++ src/api/cart.js
@@ -0,0 +1,128 @@
+import {makeApiCall} from './general';
+const CART_KEY = 'cart';
+const CACHED_CART_KEY = 'cached_processed_cart';
+const MILLIS_TO_CACHE_CART = 5 * 60 * 1000;
+
+export function getLocalCart() {
+  const rawValue = localStorage.getItem(CART_KEY);
+
+  let parsedValue = {};
+  const cartExists = (rawValue !== null);
+  if (cartExists) {
+    parsedValue.products = JSON.parse(rawValue);
+    parsedValue.productCount = parsedValue.products.length;
+  } else {
+    parsedValue = _newCart();
+  }
+
+  return parsedValue;
+}
+
+export function addProductToCart(productID) {
+  const cart = getLocalCart();
+  cart.products.push(productID);
+  _setLocalCart(cart);
+  invalidateCartCache();
+}
+
+export function removeProductFromCart(productID) {
+  const cart = getLocalCart();
+  const productIndex = cart.products.indexOf(productID);
+  if (productIndex === -1) {
+    console.warn(`Tried removing ${productID} from cart, but no such ID was found.`);
+    return;
+  }
+  cart.products.splice(productIndex, 1);
+  _setLocalCart(cart);
+  invalidateCartCache();
+}
+
+export function clearCart() {
+  _setLocalCart(_newCart());
+  invalidateCartCache();
+}
+
+function _newCart() {
+  return {
+    products: [],
+    productCount: 0,
+  };
+}
+
+function _setLocalCart(cart) {
+  localStorage.setItem(CART_KEY, JSON.stringify(cart.products));
+}
+
+export function invalidateCartCache() {
+  localStorage.removeItem(CACHED_CART_KEY);
+}
+
+export async function getFullCart() {
+  const localCart = getLocalCart();
+  const cachedValue = localStorage.getItem(CACHED_CART_KEY);
+  const cacheExists = (cachedValue !== null);
+
+  let fetchNeeded = (!cacheExists);
+  let parsedCachedValue = null;
+  if (cacheExists) {
+    parsedCachedValue = JSON.parse(cachedValue);
+
+    // Is it fresh enough? We first check if it matches the local cart
+    if (parsedCachedValue.products.length !== localCart.products.length) {
+      // Different length
+      fetchNeeded = true;
+    } else if ((parsedCachedValue.fetchedAt - Date.now()) > MILLIS_TO_CACHE_CART) {
+      // Cache has expired
+      fetchNeeded = true;
+    }
+  }
+
+  let result = null;
+  if (fetchNeeded) {
+    result = await _fetchCartFromBackend();
+    result.fetchedAt = Date.now();
+    localStorage.setItem(CACHED_CART_KEY, JSON.stringify(result));
+  } else {
+    result = parsedCachedValue;
+  }
+  return result;
+}
+
+function _fetchCartFromBackend() {
+  const query = `
+query FetchCart($products: [ID!]!) {
+  cart($products) {
+    products {
+      id
+      name
+      subtitle
+      image
+      price
+      unitPrice
+      unit
+      category
+      organic
+      percentSale {
+        cut
+      }
+      packageDeal {
+        product {
+          id
+          name
+        }
+        paidQuantity
+        minimumQuantity
+      }
+    }
+    totalBeforeDiscount
+    totalDiscount
+    total
+  }
+}
+`;
+  const variables = {
+    products: getLocalCart().products,
+  };
+  return makeApiCall(query, variables)
+    .then((body) => body.data.cart);
+}
---
+++ src/api/general.js
@@ -0,0 +1,31 @@
+const BACKEND_URL = process.env.REACT_APP_BACKEND || 'https://potato-store.herokuapp.com/graphql';
+
+function makeApiCall(query, variables, operationName) {
+  if (query === undefined) {
+    throw "query argument to makeApiCall is required";
+  }
+  const body = {'query': query};
+  if (operationName) {
+    body.operationName = operationName;
+  }
+  if (variables) {
+    body.variables = variables;
+  }
+
+  return fetch(BACKEND_URL, {
+    method: 'post',
+    headers: new Headers({
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    }),
+    body: JSON.stringify(body),
+  }).then(res => res.json())
+    .then(function(body) {
+      if (body.errors && body.errors.length) {
+        throw body;
+      }
+      return body;
+    });
+}
+
+export {makeApiCall};
