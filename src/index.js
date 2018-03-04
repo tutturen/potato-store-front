03ab85338a65bb6bc0ed07bf19fb4530fe7c6cb2
Thor Even Tutturen <thor@bustbyte.no>
2018-03-04 19:55:25 +0100

    add base name

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Default BASE_NAME suited for Github Pages
+const BASE_NAME = process.env.REACT_APP_BASE_NAME || '/potato-store-front';
+
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={BASE_NAME}>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
