f5a140923d7d31eb78d05bbf245e8cc20b437731
Thor Even Tutturen <thor@bustbyte.no>
2018-03-16 14:28:27 +0100

    remove unused files

--- src/App.test.js
+++
@@ -1,6 +0,0 @@
-// Tests are not part of this exercise. Since even the simple default test required lots of effort to maintain,
-// we have abandoned it.
-
-it('is able to run a test suite', () => {
-  console.log('Yuuuup.');
-});
