391cc549bcff5f3b709fef672f21a1460bb94ad7
Thor Even Tutturen <thor@bustbyte.no>
2018-03-16 15:41:00 +0100

    add fix for price formatter, with tests

--- src/utils/getPrice.js
+++ src/utils/getPrice.js
@@ -1,6 +1,12 @@
 function getPrice(priceFloat) {
-  const overOne = parseInt(priceFloat, 10);
-  const belowOne = Math.round((priceFloat - overOne) * 100);
+  let overOne = parseInt(priceFloat, 10);
+  let belowOne = Math.round((priceFloat - overOne) * 100);
+
+  if (belowOne > 99) {
+    overOne += Math.floor(belowOne / 100);
+    belowOne = belowOne % 100;
+  }
+
   const belowText = belowOne < 10 ? `0${belowOne}` : belowOne;
   return `kr ${overOne},${belowText}`;
 }
