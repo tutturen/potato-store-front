acba808d61f5af4d246c266e7becafe516aad4ab
Thor Even Tutturen <thor@bustbyte.no>
2018-03-23 13:05:02 +0100

    fix broken stories

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ import potatoLogo from './potato-logo.svg';
 import potatoLogoText from './potato-logo-text.svg';
 import shoppingCartIcon from './shopping-cart.svg';
 import SearchBar from './SearchBar';
-import { query as headerQuery } from '../../queries/header';
+import headerQuery from '../../queries/header';
 import logout from '../../mutations/logout';
 import './Header.css';
 
@@ -34,60 +34,49 @@ function LoggedOutMessage(props) {
   );
 }
 
-function Header(props) {
-  return (
-    <Query query={headerQuery}>
-      {({ loading, error, data }) => {
-        if (loading) {
-          return <div>Loading...</div>;
-        }
-        if (error) {
-          return <div>Error</div>;
-        }
+export function Header(props) {
+  const { loading, error, user, cartItems } = props.data;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error</div>;
+  }
 
-        return (
-          <div className="header-container">
-            <div className="header-left-content">
-              <Link to="/">
-                <img
-                  src={potatoLogo}
-                  className="potato-logo"
-                  alt="Potato Logo"
-                />
-                <img
-                  src={potatoLogoText}
-                  className="potato-logo-text"
-                  alt="Potato Store"
-                />
-              </Link>
-            </div>
-            <div className="header-middle-content">
-              <SearchBar />
-            </div>
-            <div className="header-user">
-              {data.user.loggedIn ? (
-                <LoggedInMessage user={data.user} logOut={props.logOut} />
-              ) : (
-                <LoggedOutMessage />
-              )}
-            </div>
-            <div className="header-right-content">
-              <Link to="/cart">
-                <img
-                  src={shoppingCartIcon}
-                  className="shopping-cart-icon"
-                  alt="Shopping Cart"
-                />
-                <span className="cart-size">
-                  &nbsp; Cart ({data.cartItems.length})
-                </span>
-              </Link>
-            </div>
-          </div>
-        );
-      }}
-    </Query>
+  return (
+    <div className="header-container">
+      <div className="header-left-content">
+        <Link to="/">
+          <img src={potatoLogo} className="potato-logo" alt="Potato Logo" />
+          <img
+            src={potatoLogoText}
+            className="potato-logo-text"
+            alt="Potato Store"
+          />
+        </Link>
+      </div>
+      <div className="header-middle-content">
+        <SearchBar />
+      </div>
+      <div className="header-user">
+        {user.loggedIn ? (
+          <LoggedInMessage user={user} logOut={props.logOut} />
+        ) : (
+          <LoggedOutMessage />
+        )}
+      </div>
+      <div className="header-right-content">
+        <Link to="/cart">
+          <img
+            src={shoppingCartIcon}
+            className="shopping-cart-icon"
+            alt="Shopping Cart"
+          />
+          <span className="cart-size">&nbsp; Cart ({cartItems.length})</span>
+        </Link>
+      </div>
+    </div>
   );
 }
 
-export default compose(withRouter, logout)(Header);
+export default compose(withRouter, headerQuery, logout)(Header);
