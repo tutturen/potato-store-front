bb4c097c2d43cdf607402f8ef188cd684122c360
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-03-27 15:36:46 +0200

    Show discounted price comparision for all sales in cart

--- src/pages/CartPage/CartItem/index.js
+++ src/pages/CartPage/CartItem/index.js
@@ -11,6 +11,7 @@ import './CartItem.css';
 function CartItem(props) {
   const { product, quantity, unitPrice, originalPrice } = props.item;
   const { image, name, subtitle, percentSale } = product;
+  const onSale = (unitPrice !== originalPrice);
   return (
     <div className="cart-item-container">
       <div className="cart-item-quantity-container">
@@ -35,17 +36,17 @@ function CartItem(props) {
         <div className="cart-item-description-title">{name}</div>
         <div className="cart-item-description-subtitle">{subtitle}</div>
       </div>
-      {percentSale.length > 0 && (
-        <div className="cart-item-prev-price">
-          {getPrice(originalPrice * quantity)}
-        </div>
+      {onSale && (
+        <React.Fragment>
+          <div className="cart-item-prev-price">
+            {getPrice(originalPrice * quantity)}
+          </div>
+          <div className="cart-item-sale-price">
+            {getPrice(unitPrice * quantity)}
+          </div>
+        </React.Fragment>
       )}
-      {percentSale.length > 0 && (
-        <div className="cart-item-sale-price">
-          {getPrice(unitPrice * quantity)}
-        </div>
-      )}
-      {percentSale.length === 0 && (
+      {!onSale && (
         <div className="cart-item-price">{getPrice(unitPrice * quantity)}</div>
       )}
       <div className="cart-item-delete-button">
